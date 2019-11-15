055ee137c4ab1477b5722e3ddca947eeacb21c66
Marcos Lund <mlund@itba.edu.ar>
2019-11-15 12:55:03 -0300

    Add FFT implementations

---
+++ fast_ft.m
@@ -0,0 +1,10 @@
+function y = fast_ft(x,N)
+  L=length(x);
+  y=[ ]; % matriz vacia
+  k=0:L -1;
+  for m=0:N-1
+    b=exp(-(1i*2*k*m*pi)/N);
+    s=sum(x.*b);
+    y=[y s];
+  endfor
+endfunction
---
+++ inv_fft.m
@@ -0,0 +1,10 @@
+function y = inv_fft(x,N)
+  L=length(x);
+  y=[ ]; % matriz vacia
+  k=0:L -1;
+  for m=0:N-1
+    b=exp((1i*2*k*m*pi)/N);
+    s=sum(x.*b)./N;
+    y=[y s];
+  endfor
+endfunction
