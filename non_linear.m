ed92b827838c4a1bbab168d7f8ce4d256cb561a1
Unknown <mlund@itba.edu.ar>
2019-11-19 14:25:30 -0300

    Add RK4 for non-linear solution

--- non_linear.m
+++ non_linear.m
@@ -1,6 +1,10 @@
-%linear Returns the solution for the non-linear part of the KdV equation
+% Returns the solution for the non-linear part of the KdV equation (RK4)
 function U = non_linear(U, k, h)
     %sz = size(U);
-    %U = U - (3i * k * h) .* fast_ft((real(inv_fft(U, sz(2)))).^2, sz(2)); % phi_0
-    U = U - (3i * k * h) .* fft((real(ifft(U))).^2); % phi_0
+    f = - 3i * k * h;
+    a = f .* fft(real(ifft(U)).^2);
+    b = f .* fft(real(ifft(U + a/2)).^2);
+    c = f .* fft(real(ifft(U + b/2)).^2);
+    d = f .* fft(real(ifft(U + c)).^2);
+    U = U + (a + 2*(b + c) + d) / 6; % phi_0
 end
